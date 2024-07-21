3bfb3c8ceeb29c8eded06ce85d274c7f6e40c5ec
Andrew Stroganov <savthe@gmail.com>
2024-07-21 20:11:27 +0300

    Colors update

--- nvim/lua/config/auto-dark-mode.lua
+++ nvim/lua/config/auto-dark-mode.lua
@@ -5,6 +5,32 @@ auto_dark_mode.setup({
 	set_dark_mode = function()
 		vim.api.nvim_set_option('background', 'dark')
 		vim.cmd('colorscheme dracula')
+
+    vim.cmd('set colorcolumn=')
+    vim.cmd [[hi Normal guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
+    vim.cmd [[hi CursorLineNr guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
+    vim.cmd [[hi ColorColumn guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
+    vim.cmd [[hi DiffText guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
+    vim.cmd [[hi Folded guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
+    vim.cmd [[hi FoldColumn guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
+    vim.cmd [[hi SignColumn guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
+    vim.cmd [[hi StatusLine guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
+    vim.cmd [[hi clear CursorLine]]
+    --vim.cmd [[hi CursorLine gui=underline cterm=underline guisp=gray30]]
+    vim.cmd [[hi LineNr ctermfg=gray guifg=#353535]]
+    vim.cmd [[hi TelescopeBorder guifg=#aaaaaa guibg=NONE]]
+    vim.cmd [[hi TelescopeNormal guibg=NONE]]
+    vim.cmd [[hi TelescopePreviewNormal guibg=NONE]]
+    vim.cmd [[hi TelescopePromptNormal guibg=NONE]]
+    vim.cmd [[hi TelescopeResultsNormal guibg=NONE]]
+    vim.cmd [[hi TelescopeTitle guibg=NONE]]
+    vim.cmd [[hi TelescopeSelection guibg=grey30]]
+    vim.cmd [[hi Comment guifg=#637777]]
+    vim.cmd [[hi FloatBorder guifg=#aaaaaa]]
+    vim.cmd([[highlight DiagnosticVirtualTextError guibg=NONE]])
+    vim.cmd([[highlight DiagnosticVirtualTextWarn guibg=NONE]])
+    vim.cmd([[highlight DiagnosticVirtualTextInfo guibg=NONE]])
+    vim.cmd([[highlight DiagnosticVirtualTextHint guibg=NONE]])
 	end,
 	set_light_mode = function()
 		vim.api.nvim_set_option('background', 'light')
--- nvim/lua/config/dracula.lua
+++ nvim/lua/config/dracula.lua
@@ -2,7 +2,8 @@ local dracula = require("dracula")
 dracula.setup({
 colors = {
     comment = "#637777",
-    selection = "#222922",
+    --selection = "#222922",
+    selection = "NONE",
   },
   show_end_of_buffer = true, -- default false
   -- use transparent background
@@ -28,30 +29,6 @@ colors = {
 
   overrides = function (colors)
 
-vim.cmd('set colorcolumn=')
-vim.cmd [[hi Normal guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
-vim.cmd [[hi CursorLineNr guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
-vim.cmd [[hi ColorColumn guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
-vim.cmd [[hi DiffText guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
-vim.cmd [[hi Folded guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
-vim.cmd [[hi FoldColumn guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
-vim.cmd [[hi SignColumn guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
-vim.cmd [[hi StatusLine guibg=NONE ctermbg=NONE gui=NONE cterm=NONE]]
---vim.cmd [[hi CursorLine gui=underline cterm=underline guisp=gray30]]
-vim.cmd [[hi LineNr ctermfg=gray guifg=#353535]]
-vim.cmd [[hi TelescopeBorder guifg=#aaaaaa guibg=NONE]]
-vim.cmd [[hi TelescopeNormal guibg=NONE]]
-vim.cmd [[hi TelescopePreviewNormal guibg=NONE]]
-vim.cmd [[hi TelescopePromptNormal guibg=NONE]]
-vim.cmd [[hi TelescopeResultsNormal guibg=NONE]]
-vim.cmd [[hi TelescopeTitle guibg=NONE]]
-vim.cmd [[hi TelescopeSelection guibg=grey30]]
-vim.cmd [[hi Comment guifg=#637777]]
-vim.cmd [[hi FloatBorder guifg=#aaaaaa]]
-vim.cmd([[highlight DiagnosticVirtualTextError guibg=NONE]])
-vim.cmd([[highlight DiagnosticVirtualTextWarn guibg=NONE]])
-vim.cmd([[highlight DiagnosticVirtualTextInfo guibg=NONE]])
-vim.cmd([[highlight DiagnosticVirtualTextHint guibg=NONE]])
      return {
     --   NonText = { }, -- set NonText fg to white of theme
      }
